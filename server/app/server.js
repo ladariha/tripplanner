ce37387c900d442027cdc30b388aca44c705a898
Lada Riha <riha.vladimir@gmail.com>
2014-10-19 18:50:17 +0200

    fixed absolute paths

--- server/app/server.js
+++ server/app/server.js
@@ -10,21 +10,19 @@ var server = null;
 var applicationCore = {};
 var tripPlanner = require("./core/tripPlanner");
 
-
+console.log(__dirname);
 function loadConfig(filename) {
     try {
         return JSON.parse(fs.readFileSync(filename).toString());
     } catch (err) {
-        /*console.log("There has been an error parsing your JSON Configuration (server-config.json)");
-         console.log(err);*/
-        throw "There has been an error parsing your JSON Configuration (server-config.json)\n" + err;
+        throw new Error("There has been an error parsing your JSON Configuration ("+filename+") - " + err.toString());
     }
 }
 
 (function start() {
     
     console.log("loading configuration...");
-    var config = loadConfig("./config.json") || {};
+    var config = loadConfig(path.join(__dirname, "config.json")) || {};
     
     console.log("loading sources...");
     applicationCore.tripPlanner = tripPlanner;
