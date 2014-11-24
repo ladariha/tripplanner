62ca1516c36d4587323bd69ced2d7323de32e12b
Lada Riha <riha.vladimir@gmail.com>
2014-11-24 21:55:33 +0100

    getting ready for server-side authentication

--- server/src/server.js
+++ server/src/server.js
@@ -9,20 +9,26 @@ var app = express();
 var server = null;
 var applicationCore = {};
 var tripPlanner = require("./core/tripPlanner");
+var session = require('express-session');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
 
 function loadConfig(filename) {
     try {
         return JSON.parse(fs.readFileSync(filename).toString());
     } catch (err) {
-        throw new Error("There has been an error parsing your JSON Configuration ("+filename+") - " + err.toString());
+        throw new Error("There has been an error parsing your JSON Configuration (" + filename + ") - " + err.toString());
     }
 }
 
 (function start() {
-    
+
     console.log("loading configuration...");
     var config = loadConfig(path.join(__dirname, "config.json")) || {};
-    
+
     console.log("loading sources...");
     applicationCore.tripPlanner = tripPlanner;
     // connect to DB & setup models
@@ -35,6 +41,18 @@ function loadConfig(filename) {
         next();
     });
 
+    // login
+    // require('./config/passport')(passport); // pass passport for configuration
+    app.use(morgan('dev')); // log every request to the console
+    app.use(cookieParser()); // read cookies (needed for auth)    
+    app.use(session({secret: 'ilovescotchscotchyscotchscotch'})); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+
+
     app.use(bodyParser.json());       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({
         extended: true
@@ -44,7 +62,7 @@ function loadConfig(filename) {
     var routes = utils.listFiles((path.join(path.dirname(__filename), config.server.paths.api)).toString());
     // load all routers
     routes.forEach(function (route) {
-        require(route).registerRoute(app);
+        require(route).registerRoute(app, passport);
     });
 
 
