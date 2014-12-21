4f598e32b4a6d99162279088133040db2c73e092
Lada Riha <riha.vladimir@gmail.com>
2014-12-21 12:29:11 +0100

    Date's method for easy conversion

--- server/client/js/shims.js
+++ server/client/js/shims.js
@@ -10,4 +10,19 @@
     Date.prototype.toPrettyString = function () {
         return this.getDate() + " " + monthsNames.fullName[this.getMonth()] + " " + this.getFullYear();
     };
+    
+    /**
+     * Converts from "03-December-2014" to Date object 
+     * @param {type} dateString
+     * @returns {Date.prototype}
+     */
+    Date.prototype.getFromInput = function(dateString){
+        var values = dateString.split("-");
+        this.setDate(parseInt(values[0], 10));
+        this.setFullYear(parseInt(values[2], 10));
+        this.setMonth(monthsNames.fullName.indexOf(values[1]));
+        return this;
+        
+    };
+
 })();
