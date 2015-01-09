14fe25fa5441feca67c90031f9d2406a2f6d5127
Lada Riha <riha.vladimir@gmail.com>
2015-01-09 22:19:42 +0100

    refactoring

--- server/client/js/core/cache.js
+++ server/client/js/core/cache.js
@@ -5,35 +5,24 @@ angular.module("tripPlanner.core", ["tripPlanner.utils", "tripPlanner.user"])
 
             var cache = {
                 get: function (key) {
-                    if (window.localStorage) {
-                        return JSON.parse(window.localStorage.getItem(key));
-                    }
-                    return null;
+                    return JSON.parse(window.localStorage.getItem(key));
                 },
                 set: function (key, value) {
-                    if (window.localStorage) {
+                    value = JSON.stringify(value);
+                    window.localStorage.removeItem(key);
+                    try {
+                        window.localStorage.setItem(key, value);
+                    } catch (e) {
+                        window.localStorage.clear();
+                        window.localStorage.setItem(key, value);
 
-                        value = JSON.stringify(value);
-                        window.localStorage.removeItem(key);
-                        try {
-                            window.localStorage.setItem(key, value);
-                        } catch (e) {
-                            if (e.code >= 20 && e.code <= 22) {
-                                this.drop();
-                                window.localStorage.setItem(key, value);
-                            }
-                        }
                     }
                 },
                 remove: function (key) {
-                    if (window.localStorage) {
-                        window.localStorage.removeItem(key);
-                    }
+                    window.localStorage.removeItem(key);
                 },
                 drop: function () {
-                    for (var i = 0, max = window.localStorage.length; i < max; i++) {
-                        window.localStorage.removeItem(window.localStorage.key(0));
-                    }
+                    window.localStorage.clear();
                 }
             };
 
