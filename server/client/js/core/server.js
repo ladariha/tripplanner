6f9272707e3394dad8d6051ee375f1061faeca2a
Lada Riha <riha.vladimir@gmail.com>
2015-01-09 22:26:18 +0100

    refactoring

--- server/client/js/core/server.js
+++ server/client/js/core/server.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module("tripPlanner.core")
-        .factory("tp.core.Server", [function() {
+        .factory("tp.core.Server", [function () {
                 function Server(endpoints, pattern) {
                     this.endpoints = endpoints;
                     window.console.log("server created");
@@ -9,27 +9,23 @@ angular.module("tripPlanner.core")
                 }
 
                 Server.prototype._patterns = {};
-                Server.prototype._patterns.php = function(endpoint, parameters) {
+                Server.prototype._patterns.php = function (endpoint, parameters) {
                     var url = "";
                     for (var parameter in parameters) {
-                        if (parameters.hasOwnProperty(parameter)) {
-                            url += "&" + parameter + "=" + encodeURIComponent(parameters[parameter]);
-                        }
+                        url += "&" + parameter + "=" + encodeURIComponent(parameters[parameter]);
                     }
                     return (url.length > 1) ? this.endpoints[endpoint] + "?" + url : this.endpoints[endpoint];
                 };
 
-                Server.prototype._patterns.prettyUrl = function(endpoint, parameters) {
+                Server.prototype._patterns.prettyUrl = function (endpoint, parameters) {
                     var url = "";
                     for (var parameter in parameters) {
-                        if (parameters.hasOwnProperty(parameter)) {
-                            url += "/" + encodeURIComponent(parameters[parameter]);
-                        }
+                        url += "/" + encodeURIComponent(parameters[parameter]);
                     }
                     return this.endpoints[endpoint] + url;
                 };
 
-                Server.prototype.buildURL = function(endpoint, parameters) {
+                Server.prototype.buildURL = function (endpoint, parameters) {
                     return this._patterns[this.preferredPattern].call(this, endpoint, parameters);
                 };
 
