f5d525cfaabde0101c9ae2d5788a0442d6cca53e
Lada Riha <riha.vladimir@gmail.com>
2015-01-09 21:09:30 +0100

    refactoring

--- server/client/js/core/cache.js
+++ server/client/js/core/cache.js
@@ -1,66 +1,41 @@
 "use strict";
 
 angular.module("tripPlanner.core", ["tripPlanner.utils", "tripPlanner.user"])
-        .factory("tp.core.Cache", function() {
+        .factory("tp.core.Cache", function () {
 
-            function Cache() {
-                this.defaultCookiesExpiration = 12 * 60 * 60 * 1000;
-            }
-
-            Cache.prototype.setCookie = function(key, value) {
-                var date = new Date();
-                date.setTime(date.getTime() + (this.defaultCookiesExpiration));
-                var expires = "; expires=" + date.toGMTString();
-                window.document.cookie = key + "=" + value + expires + "; path=/";
-            };
-            Cache.prototype.removeCookie = function(key) {
-                window.document.cookie = key + "=;; path=/";
-            };
-
-            Cache.prototype.getCookie = function(key) {
-                key += "=";
-                var ca = window.document.cookie.split(";");
-                for (var i = 0; i < ca.length; i++) {
-                    var c = ca[i];
-                    while (c.charAt(0) === " "){
-                        c = c.substring(1, c.length);
+            var cache = {
+                get: function (key) {
+                    if (window.localStorage) {
+                        return JSON.parse(window.localStorage.getItem(key));
                     }
-                    if (c.indexOf(key) === 0) {
-                        return c.substring(key.length, c.length);
-                    }
-                }
-            };
+                    return null;
+                },
+                set: function (key, value) {
+                    if (window.localStorage) {
 
-            Cache.prototype.get = function(key) {
-                if (window.localStorage) {
-                    return JSON.parse(window.localStorage.getItem(key));
-                }
-                return null;
-            };
-            Cache.prototype.set = function(key, value) {
-                if (window.localStorage) {
-                    window.localStorage.removeItem(key);
-                    try {
-                        window.localStorage.setItem(key, JSON.stringify(value));
-                    } catch (e) {
-                        if (e.code === 22 || e.code === 21 || e.code === 20) {
-                            this.drop();
-                            window.localStorage.setItem(key, JSON.stringify(value));
+                        value = JSON.stringify(value);
+                        window.localStorage.removeItem(key);
+                        try {
+                            window.localStorage.setItem(key, value);
+                        } catch (e) {
+                            if (e.code >= 20 && e.code <= 22) {
+                                this.drop();
+                                window.localStorage.setItem(key, value);
+                            }
                         }
                     }
-                }
-            };
-            Cache.prototype.remove = function(key) {
-                if (window.localStorage) {
-                    window.localStorage.removeItem(key);
+                },
+                remove: function (key) {
+                    if (window.localStorage) {
+                        window.localStorage.removeItem(key);
+                    }
+                },
+                drop: function () {
+                    for (var i = 0, max = window.localStorage.length; i < max; i++) {
+                        window.localStorage.removeItem(window.localStorage.key(0));
+                    }
                 }
             };
 
-            Cache.prototype.drop = function() {
-                for (var i = 0, max = window.localStorage.length; i < max; i++) {
-                    window.localStorage.removeItem(window.localStorage.key(0));
-                }
-            };
-
-            return Cache;
+            return cache;
         });
