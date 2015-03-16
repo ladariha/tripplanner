bf7377aa57424cab0937ceeb1ccfdc581771d0ec
Lada Riha <riha.vladimir@gmail.com>
2015-03-16 21:53:59 +0100

    focusing textAngular on render

--- server/client/js/ext_note/controller.js
+++ server/client/js/ext_note/controller.js
@@ -1,26 +1,29 @@
 "use strict";
 angular.module("tripPlanner.extension.note")
-        .controller("tp.ext.note.NoteCtrl", ["$scope","$stateParams","$state", "tp.ext.note.NoteHandler","tp.ext.note.NoteModel",
-            function NoteCtrl($scope, $stateParams, $state, NoteHandler, Note) {
+        .controller("tp.ext.note.NoteCtrl", [
+            "$scope", "$stateParams", "$state", "tp.ext.note.NoteHandler", "tp.ext.note.NoteModel", "textAngularManager", "$timeout",
+            function NoteCtrl($scope, $stateParams, $state, NoteHandler, Note, textAngularManager, $timeout) {
                 var tripDay = $stateParams.tripDay;
                 var noteHandler = new NoteHandler();
-                
+
                 $scope.dayName = tripDay.name;
                 $scope.localDate = tripDay.localDate.toPrettyString();
                 $scope.note = new Note(tripDay.tripId, tripDay.id);
                 $scope.create = create;
                 $scope.cancel = cancel;
-                
-                
-                function create(){
+
+                $timeout(function () {
+                    textAngularManager.retrieveEditor("note").scope.displayElements.text.trigger("focus");
+                },100);
+
+                function create() {
                     noteHandler.create($scope.note);
                 }
-                
-                
-                function cancel(){
+
+                function cancel() {
                     $state.go("trip.view", {"id": tripDay.tripId, noCache: false}, {reload: true});
                 }
-                
+
 
             }
 
