6d102bde193540b317aab6eb208b189572afd09e
Lada Riha <riha.vladimir@gmail.com>
2015-04-04 14:34:23 +0200

    added sanitizer

---
+++ server/src/misc/sanitizer.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var sanitizer = require("sanitizer");
+/**
+ * Removes unsafe tags and attributes from html
+ * @param {String} inputData
+ * @returns {String} stripped String
+ */
+String.prototype.sanitize = function () {
+    sanitizer.sanitize(this);
+};
+
+/**
+ * Removes unsafe tags and attributes from html
+ * @param {String} inputData
+ * @returns {String} stripped String
+ */
+exports.sanitize = function (inputData) {
+    return sanitizer.sanitize(inputData);
+};
