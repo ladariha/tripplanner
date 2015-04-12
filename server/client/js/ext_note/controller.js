92191199d7c34ad51e516eab77d77ed85430f563
Lada Riha <riha.vladimir@gmail.com>
2015-04-12 15:44:12 +0200

    use returned object and add it to cached trip

--- server/client/js/ext_note/controller.js
+++ server/client/js/ext_note/controller.js
@@ -1,8 +1,8 @@
 "use strict";
 angular.module("tripPlanner.extension.note")
         .controller("tp.ext.note.NoteCtrl", [
-            "$scope", "$stateParams", "$state", "tp.ext.note.NoteHandler", "tp.ext.note.NoteModel", "textAngularManager", "$timeout",
-            function NoteCtrl($scope, $stateParams, $state, NoteHandler, Note, textAngularManager, $timeout) {
+            "$scope", "$stateParams", "$state", "tp.ext.note.NoteHandler", "tp.ext.note.NoteModel", "textAngularManager", "$timeout", "tp.trip.TripCache",
+            function NoteCtrl($scope, $stateParams, $state, NoteHandler, Note, textAngularManager, $timeout, TripCache) {
                 var tripDay = $stateParams.tripDay;
                 var noteHandler = new NoteHandler();
 
@@ -14,12 +14,23 @@ angular.module("tripPlanner.extension.note")
 
                 $timeout(function () {
                     textAngularManager.retrieveEditor("note").scope.displayElements.text.trigger("focus");
-                },100);
+                }, 100);
 
                 function create() {
-                    noteHandler.create($scope.note).then(function(){
-                         $state.go("trip.view", {"id": tripDay.tripId, noCache: true}, {reload: true});
+                    noteHandler.create($scope.note).then(function (ext) {
+                        addCreatedNoteToDay(ext);
+                        $state.go("trip.view", {"id": tripDay.tripId, noCache: false}, {reload: true});
                     }, $scope.handleGenericError);
+                }
+
+                function addCreatedNoteToDay(extensionData) {
+                    var trip = TripCache.get();
+                    for (var i = 0, max = trip.days.length; i < max; i++) {
+                        if (trip.days[i].id === tripDay.id) {
+                            trip.days[i].data.push(extensionData);
+                            return;
+                        }
+                    }
                 }
 
                 function cancel() {
